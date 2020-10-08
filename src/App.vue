2f98e7b6b0b4c4b15a68dde82d808b15eb2d8f37
khanh-hust <noiphet97@gmail.com>
2020-10-08 22:19:37 +0700

    add rightclick

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Slider></Slider> -->
+
+    <!-- contact menu -->
+    <div id="context-menu">
+      <div class="item">
+        <i class="fa fa-cut"></i> Cut
+      </div>
+      <div class="item">
+        <i class="fa fa-clone"></i> Copy
+      </div>
+      <div class="item">
+        <i class="fa fa-paste"></i> Paste
+      </div>
+      <div class="item">
+        <i class="fa fa-trash-o"></i> Delete
+      </div>
+      <hr>
+      <div class="item">
+        <i class="fa fa-refresh"></i> Reload
+      </div>
+      <div class="item">
+        <i class="fa fa-times"></i> Exit
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+// import Slider from './components/Slider'
+import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
+    // Slider,
     HelloWorld
   }
 }
+window.addEventListener("contextmenu",function(event){
+  event.preventDefault();
+  var contextElement = document.getElementById("context-menu");
+  contextElement.style.top = event.offsetY + "px";
+  contextElement.style.left = event.offsetX + "px";
+  contextElement.classList.add("active");
+});
+window.addEventListener("click",function(){
+  document.getElementById("context-menu").classList.remove("active");
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  height: 100vh;
+}
+body {
+  margin:0px;
+  font-family:"Open Sans",sans-serif;
+}
+#context-menu {
+  position:fixed;
+  z-index:10000;
+  width:150px;
+  background:#1b1a1a;
+  border-radius:5px;
+  transform:scale(0);
+  transform-origin:top left;
+}
+#context-menu.active {
+  transform:scale(1);
+  transition:transform 300ms ease-in-out;
+}
+#context-menu .item {
+  padding:8px 10px;
+  font-size:15px;
+  color:#eee;
+}
+#context-menu .item:hover {
+  background:#555;
+}
+#context-menu .item i {
+  display:inline-block;
+  margin-right:5px;
+}
+#context-menu hr {
+  margin:2px 0px;
+  border-color:#555;
 }
 </style>
